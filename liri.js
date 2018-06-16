0ddbe60e5122fb93732355700b83f2ee37528f89
Aaron Colbert <aaron-colbert@hotmail.com>
2018-06-16 19:00:44 -0400

    Finished do what i say

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require("request");
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -23,113 +24,126 @@ for (var i = 3; i < len; i++)
 param = param.trim();
 console.log(command);
 console.log(param)
-
-switch (command)
+var liri = function ()
 {
-    case 'my-tweets':
-        client.get('statuses/user_timeline', function (error, tweets, response)
-        {
-            if (error) throw error;
-
-            var myTweets = eval(tweets)
-            var len = tweets.length;
-
-            console.log("Your last 20 Tweets are:")
-            for (var i = 0; i < len; i++)
+    switch (command)
+    {
+        case 'my-tweets':
+            client.get('statuses/user_timeline', function (error, tweets, response)
             {
-                console.log("\n------------------------------------------------");
-                console.log("Tweet: " + myTweets[i].text);
-                console.log("Date Posted: " + myTweets[i].created_at)
-                console.log("------------------------------------------------");
-            }
-        });
-        break;
+                if (error) throw error;
 
-    case 'spotify-this-song':
+                var myTweets = eval(tweets)
+                var len = tweets.length;
 
-        if (param == "")
-        {
-            param = "The Sign"// THe Sign by Ace of Base
-        }
-        spotify.search({ type: 'track', query: 'The Sign' })
-            .then(function (response)
-            {
-                var songName = "";
-                var artist = "";
-                var link;
-                var album;
-
-                console.log(response.tracks.items);
-
-                console.log("------------------------------------------------");
-                console.log("Artist(s): " + artist);
-                console.log("Song Name: " + songName);
-                console.log("Preview: " + link);
-                console.log("Album: " + album);
-                console.log("------------------------------------------------");
-
-
-            })
-            .catch(function (err)
-            {
-                console.log(err);
-            });
-        break;
-
-    case 'movie-this'://Song name id process.argv 3to infinit string 
-
-        if (param == "")
-        {
-            param = "Mr. Nobody";
-        }
-        var queryUrl = "http://www.omdbapi.com/?t=" + param + "&y=&plot=short&apikey=trilogy";
-
-        console.log(queryUrl);
-
-        request(queryUrl, function (error, response, body)
-        {
-            if (!error && response.statusCode === 200)
-            {
-                console.log("------------------------------------------------");
-                console.log("Title: " + JSON.parse(body).Title);
-                console.log("Year Released: " + JSON.parse(body).Year);
-                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-
-                var len = JSON.parse(body).Ratings.length;
-                var rating = JSON.parse(body).Ratings
-                var tomatoRating;
+                console.log("Your last 20 Tweets are:")
                 for (var i = 0; i < len; i++)
                 {
-                    if (rating[i].Source == "Rotten Tomatoes")
-                    {
-                        tomatoRating = rating[i].Value;
-                    }
+                    console.log("\n------------------------------------------------");
+                    console.log("Tweet: " + myTweets[i].text);
+                    console.log("Date Posted: " + myTweets[i].created_at)
+                    console.log("------------------------------------------------");
                 }
-                if (!tomatoRating)
-                {
-                    tomatoRating = "No Rating Available";
-                }
+            });
+            break;
 
-                console.log("Rotten Tomatos Rating: " + tomatoRating);
-                console.log("Country: " + JSON.parse(body).Country);
-                console.log("Language: " + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors and/or Actresses: " + JSON.parse(body).Actors);
-                console.log("------------------------------------------------");
-                console.log("The Release Year is: " + JSON.stringify(JSON.parse(body), null, 2));
+        case 'spotify-this-song':
+
+            if (param == "")
+            {
+                param = "The Sign"// THe Sign by Ace of Base
             }
-        });
-        break;
+            spotify.search({ type: 'track', query: 'The Sign' })
+                .then(function (response)
+                {
+                    var songName = "";
+                    var artist = "";
+                    var link;
+                    var album;
 
-    case 'do-what-it-says':
-        //Read random command from random.txt 
-        //txt default spotify-this-song,"I Want it That Way"
+                    console.log(response.tracks.items);
 
+                    console.log("------------------------------------------------");
+                    console.log("Artist(s): " + artist);
+                    console.log("Song Name: " + songName);
+                    console.log("Preview: " + link);
+                    console.log("Album: " + album);
+                    console.log("------------------------------------------------");
 
-        break;
-    default:
+                })
+                .catch(function (err)
+                {
+                    console.log(err);
+                });
+            break;
 
+        case 'movie-this'://Song name id process.argv 3to infinit string 
+
+            if (param == "")
+            {
+                param = "Mr. Nobody";
+            }
+            var queryUrl = "http://www.omdbapi.com/?t=" + param + "&y=&plot=short&apikey=trilogy";
+
+            console.log(queryUrl);
+
+            request(queryUrl, function (error, response, body)
+            {
+                if (!error && response.statusCode === 200)
+                {
+                    console.log("------------------------------------------------");
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Year Released: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+
+                    var len = JSON.parse(body).Ratings.length;
+                    var rating = JSON.parse(body).Ratings
+                    var tomatoRating;
+                    for (var i = 0; i < len; i++)
+                    {
+                        if (rating[i].Source == "Rotten Tomatoes")
+                        {
+                            tomatoRating = rating[i].Value;
+                        }
+                    }
+                    if (!tomatoRating)
+                    {
+                        tomatoRating = "No Rating Available";
+                    }
+
+                    console.log("Rotten Tomatos Rating: " + tomatoRating);
+                    console.log("Country: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors and/or Actresses: " + JSON.parse(body).Actors);
+                    console.log("------------------------------------------------");
+                    console.log("The Release Year is: " + JSON.stringify(JSON.parse(body), null, 2));
+                }
+            });
+            break;
+
+        case 'do-what-it-says':
+            fs.readFile("random.txt", "utf8", function (error, data)
+            {
+                if (error)
+                {
+                    return console.log(error);
+                }
+                console.log(data);
+                console.log(dataArr);
+                var dataArr = data.split(",");
+                command = dataArr[0];
+                param = dataArr[1];
+                liri();
+
+            });
+            break;
+        default:
+    }
 }
+
+liri();
+
 
 
 
